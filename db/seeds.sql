3ccf96ae45b404c0f2f481da5da882ab3890cfb6
Katri Morrison-Goulias <katrimorrison-goulias@Konstadinoss-MacBook-Air.local>
2018-08-09 18:07:50 -0700

    added new things

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', true);
