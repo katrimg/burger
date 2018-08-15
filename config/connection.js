eda62cefc33d99773e8097f46cc86e36bb647dbf
Katri Morrison-Goulias <katrimorrison-goulias@Konstadinoss-MacBook-Air.local>
2018-08-15 12:26:55 -0700

    added code for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,30 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3000,
   user: "root",
   password: "",
-  //changed database name
   database: "burgers_db"
 });
+};
+
+
 
 // Make connection.
-connection.connect(function(err) {
+  connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
 });
-
+  
 // Export connection for our ORM to use.
 module.exports = connection;
